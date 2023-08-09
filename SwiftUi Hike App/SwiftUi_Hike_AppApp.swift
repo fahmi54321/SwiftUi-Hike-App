8a19d084c89b2bdf8784eddcc552c2952d6e865e
fahmi <fahmiabdulaziz44@gmail.com>
2023-08-09 20:34:07 +0700

    Initial Commit

---
+++ SwiftUi Hike App/SwiftUi_Hike_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_Hike_AppApp.swift
+//  SwiftUi Hike App
+//
+//  Created by Fahmi Aziz on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_Hike_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
